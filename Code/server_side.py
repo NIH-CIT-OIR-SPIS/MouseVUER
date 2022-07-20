bb3105b08a3d1c45e93d45d94963ce001db6257c
yeric1789 <76454253+yeric1789@users.noreply.github.com>
2022-07-20 15:32:16 -0400

    Server Cont

--- Code/server_side.py
+++ Code/server_side.py
@@ -1,10 +1,20 @@
+#!/usr/bin/python3
+from _thread import start_new_thread
 import os
 import socket    
 import multiprocessing
 import subprocess as sp
 import json
 import time
-
+import threading
+import argparse
+import signal
+import ipaddress
+import platform
+import logging
+import selectors
+import types
+import sys
 
 """
 This is the server side of the application.
@@ -25,16 +35,16 @@ STARTING UP FFMPEG LISTENER WITH THREADED COMMAND LINE CALL
 8) Then this server will then call a subprocess command in a parallel process. This command will contain ffmpeg rtsp listener and the ip address 
 
 
-9) Then it will send a message with a command string attached to all of the clients.
+9) Then it will send a message with the servers ip address, and with a command string attached to all of the clients.
 
 
 10) Then clients will run this command string in a subprocess on their own machine
 
 11) The server will wait for a end signal from the subprocess command ffmpeg rtsp listener, once seen it will close the subprocess and send a message to all of the clients to end the session.
 
-
 """
-
+  
+NO_FORCE_EXIT = True
 
 
 
@@ -115,9 +125,154 @@ def map_network(pool_size=255):
 
     return ip_list
 
+def port_type(port: int):
+    port = int(port)
+    if port <= 1024 or port > 65535:
+        raise argparse.ArgumentTypeError("Error: Port number must in the ranges [1025, 65535]. Port number given: {}".format(port))
+    return port
+
+def validate_ip(addr):
+    try:
+        ip = ipaddress.ip_address(addr)
+        print("Valid IP: {}".format(ip))
+        return addr
+    except ValueError:
+        #print("IP address {} is not valid".format(addr))
+        raise argparse.ArgumentTypeError("IP address {} is not valid".format(addr))
+        return None
+
+# class ServerWorker:
+#     def __init__(self, clientinfo):
+#         self.clientinfo = clientinfo
+
+#     def run(self):
+#         threading.Thread(target=self.requesting).start()
+
+
+#     def requesting(self):
+#         """ Recive Request from client """
+#         connsocket = self.clientinfo['connsocket'][0]
+#         while True:
+#             data = connsocket.recv(1024)
+#             if data:
+#                 print("Received: {}".format(data))
+#                 self.clientinfo['request'] = data.decode('utf-8')
+#                 break
+
+
+class Server:
+    """
+    Server class
+    
+    """
+    def __init__(self, ip_lst_in, **kwargs):
+        self.client_ip_lst = ip_lst_in
+        self.argdict = kwargs
+        self.port = self.argdict['port']
+        self.ffmpeg_port_map = {} # maps ip address to two distinct ports
+        self.host_port = {}
+        self.server_ip = get_my_ip()
+        i = 0
+        port_store = self.port
+        for ip_addr in self.client_ip_lst:
+            
+            self.ffmpeg_port_map.update({ip_addr: (port_store + i, port_store + i + 1)})
+            i += 2
+
+        for ip_addr in self.client_ip_lst:
+            self.host_port.update({ip_addr: port_store + i})
+            i += 1
+        i = 0
+        self.print_lock = threading.Lock()
+        # for key, value in kwargs.items():
+        #     self.argdict[key] = value
+        self.logger = logging.getLogger(__name__)
+        self.logger.setLevel(logging.DEBUG)
+        self.logger.addHandler(logging.StreamHandler())
+        self.logger.info("Server started with ip address {} ".format(self.server_ip))
+        self.logger.info("Client IP List: {}".format(self.client_ip_lst))
+        self.logger.info("Arguments: {}".format(self.argdict))
+        self.logger.info("Platform: {}".format(platform.platform()))
+        self.logger.info("OS: {}".format(platform.system()))
+        self.logger.info("OS Version: {}".format(platform.version()))
+
+
+    def start_conn_send_data(self, host: str, port: int):
+        """
+        Start a connection to a host and send data
+        :param host:
+        :param port:
+        :return:
+        """
+        ssocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        ssocket.bind((host, port))
+        ssocket.listen(5)
+        while NO_FORCE_EXIT:
+            c, addr = ssocket.accept()
+            self.print_lock.acquire()
+            print("Connected to {}:{}".format(addr[0], addr[1]))
+            start_new_thread(self.get_info, (self, c, addr))
+
+    def get_info(self, connsocket, addr):
+        while True:
+            data = connsocket.recv(1024)
+            if not data:
+
+                
+                print("Received: {}".format(data))
+                return data.decode('utf-8')
+            
+
+
+
+
+
+
+
+
+
+def server_side_command_line_parser():
+    """
+    Takes in the options of what ports will be used, what level of verbosity for the logging, 
+    Options:
+    -p, --port <int> must be between values [1025, 65535]
+    -v, --verbose <string> (debug, info, error, warning, quiet)
+    -h, --help
+    -dim, --dimensions <string> (widthxheight) valid dimensions are: (640x480, 1280x720, 1920x1080). Default is 1280x720
+    -f, --framerate <int> valid framerates are: (15, 30, 60, 90). Default is 30. 60 fps and 90 fps are only available for 640x480. 
+                            30 fps and 15 fps are available for all dimensions.
+    -time <time> amount of time to record in seconds
+    -json <json file> json file to be used for the video recordings.
+    -split_time <time> length in time of each video recording in seconds. Default is 120 seconds.
+    Optional:
+    -basename <string> the base name of the video files
+    -max_depth <int> the max depth of for the depth camera to use. Must be greater than min_depth. Valid range is [1, 65535]. Default is 65535
+    -min_depth <int> the min depth for the depth camera to use. Must be less than max_depth. Valid range is [0, 65534]. Default is 0
+    -depth_unit <int> The units of the depth camera to use valid range, [40, 25000]. Default value is 1000
+    """
+    parser = argparse.ArgumentParser(description='Server for the video streaming application')
+    parser.add_argument('--port', type=int, default=5000, help='Port to listen on.  Must be between values [1025, 65000]')
+    parser.add_argument('--verbose', type=str, default='info', help='Verbosity level of the logging')
+    parser.add_argument('--dimensions', type=str, default='1280x720', help='Dimensions of the video stream, (widthxheight) valid dimensions are: (640x480, 1280x720, 1920x1080). Default is 1280x720')
+    parser.add_argument('--fps', type=int, default=30, help='Framerate of recordings.Valid framerates are: (15, 30, 60, 90). Default is 30. 60 fps and 90 fps are only available for 640x480. 30 fps and 15 fps are available for all dimensions.')
+    parser.add_argument('-time', type=int, default=30, help='Amount of time to record in seconds. Default 30 seconds')
+    parser.add_argument('--json', type=str, default='', help='Json file to be sent to the client and used for the video recordings')
+    parser.add_argument('--split_time', type=int, default=120, help='Length in to split each recording into. Default is 120 seconds')
+
+    parser.add_argument('-basename', '--basename', type=str, default='test_', help='The base name of the video files')
+    parser.add_argument('-max_depth', '--max_depth', type=int, default=65535, help='The max depth of for the depth camera to use. Must be greater than min_depth. Valid range is [1, 65535]. Default is 65535')
+    parser.add_argument('-min_depth', '--min_depth', type=int, default=0, help='The min depth for the depth camera to use. Must be less than max_depth. Valid range is [0, 65534]. Default is 0')
+    parser.add_argument('-depth_unit', '--depth_unit', type=int, default=1000, help='The units of the depth camera to use valid range, [40, 25000]. Default value is 1000')
+
+    return parser.parse_args()
 
 if __name__ == '__main__':
-    lst = map_network()
+    print("Getting IP addresses...")
+    ip_lst = map_network()
+    print("Done getting IP addresses")
+    args = server_side_command_line_parser()
+    server = Server(ip_lst, **vars(args))
+    
     #lst.remove()
-    print(lst)
+    #print(lst)
 
