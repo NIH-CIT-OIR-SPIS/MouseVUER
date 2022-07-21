54788a6a008b9013eca79b5d1e8795ace8fa9b3c
yeric1789 <76454253+yeric1789@users.noreply.github.com>
2022-07-21 16:09:53 -0400

    Added to server

--- Code/make_ssl_keys_cert.py
+++ Code/make_ssl_keys_cert.py
@@ -4,10 +4,10 @@ import os
 COMMON_NAME = "SCHORE_SERVER"
 ORGANIZATION = "NIH"
 COUNTRY_ORGIN = "US"
-if os.path.exists("keys/"):
+if os.path.isdir("keys/"):
     os.system("rm -rf keys/")
     print("Making new keys and certificates...")
-    sp.run('mkdir -p keys/', shell=True)
+    os.system("mkdir keys/")
 
 sp.run("openssl req -new -newkey rsa:2048 -days 5000 -nodes -x509 -subj '/CN=" + COMMON_NAME + "/O=" + ORGANIZATION + " LTD./C=" + COUNTRY_ORGIN +  "' -keyout  keys/server.key -out keys/server.crt ", shell=True) # Generate a self-signed certificate
 sp.run("openssl req -new -newkey rsa:2048 -days 5000 -nodes -x509 -subj '/CN=" + COMMON_NAME + "/O=" + ORGANIZATION + " LTD./C=" + COUNTRY_ORGIN +  "' -keyout  keys/client.key -out keys/client.crt ", shell=True) # Generate a self-signed certificate
