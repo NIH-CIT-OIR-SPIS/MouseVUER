c09ee4a4d1788b908569bc8d4fd02ea8112c8b6f
yeric1789 <76454253+yeric1789@users.noreply.github.com>
2022-08-16 10:44:25 -0400

    Added to extract_ffmpeg_frames and collect_raw

--- Collect_Training_Code/extract_ffmpeg_frames.py
+++ Collect_Training_Code/extract_ffmpeg_frames.py
@@ -6,6 +6,9 @@ import subprocess as sp
 import multiprocessing
 import time
 import shlex
+import signal 
+import sys
+import platform
 
 HEIGHT = 720
 WIDTH = 1280
@@ -17,46 +20,28 @@ def run_processes_parallel(cmd_lst):
     """
     Start up two server processes in parallel. Send them both signals to terminate.
     """
-    
     try:
         p_list = []
         for cmd in cmd_lst:
-            
-            p = sp.Popen(shlex.split(cmd),  shell=False)
-            p_list.append(p)
-            #p_list.append(sp.Popen(cmd, shell=False))
+            p_list.append(sp.Popen(cmd, shell=True))
         
         # Wait for the processes to terminate
-        i = True
-        while i:
-            for p in p_list:
-                if p.poll() is None:
-                    time.sleep(0.1)
-                else:
-                    i = False
-            #p.wait()
-            #p.wait()
+        for p in p_list:
+            p.wait()
     
     # Send the processes a signal to terminate
-    except KeyboardInterrupt:
-        for p in p_list:
-            p.send_signal(signal.SIGINT)
-            p.wait()
-        sys.exit(0)
     finally:
         if p_list is not []:
             for p in p_list:
-                if p is not None:
-                    p.send_signal(signal.SIGQUIT)
-
+                p.send_signal(signal.SIGQUIT)
 
 def extract_frame_command(input_vid, frame_num, output_dir):
-    command = 'ffmpeg -i {:s} -vframes {:d} {:s}/depth_frame_%06d.tif'.format(input_vid, frame_num, output_dir)
+    command = 'ffmpeg -loglevel error -nostats -y -threads 2 -i {:s} -vframes {:d} {:s}/depth_frame_%06d.tif'.format(input_vid, frame_num, output_dir)
     #sp.call(command, shell=True)
     return command
 
 def extract_rgb_frame_command(input_vid_mp4, frame_num, output_dir):
-    command = 'ffmpeg -i {:s} -vframes {:d} {:s}/rgb_frame_%06d.tif'.format(input_vid_mp4, frame_num, output_dir)
+    command = 'ffmpeg -loglevel error -nostats -y -threads 2 -i {:s} -vframes {:d} {:s}/rgb_frame_%06d.tif'.format(input_vid_mp4, frame_num, output_dir)
     return command
 
 def options():
@@ -76,16 +61,18 @@ def main():
     cmd_lst = []
     cmd_lst.append(extract_frame_command(input_vid, frame_num, output_dir))
     cmd_lst.append(extract_rgb_frame_command(input_vid_mp4, frame_num, output_dir))
-    p1 = multiprocessing.Process(target=run_processes_parallel, args=(cmd_lst,))
-    p1.start()
-    try:
-        p1.join()
-    except Exception as e:
-        p1.terminate()
-        p1.join()
-        print("Error here: {}".format(e))
-        #pass
-    os.system("stty echo")
+    # p1 = multiprocessing.Process(target=run_processes_parallel, args=(cmd_lst,))
+    # p1.start()
+    # try:
+    #     p1.join()
+    # except Exception as e:
+    #     p1.terminate()
+    #     p1.join()
+    #     print("Error here: {}".format(e))
+    #     #pass
+    run_processes_parallel(cmd_lst)
+    if platform.system() == 'Linux':
+        os.system("stty echo")
 
 
 if __name__ == '__main__':
