213c6e895758bbb56f0350e67df1e9c0e0e28dbd
yeric1789 <76454253+yeric1789@users.noreply.github.com>
2022-08-16 10:51:30 -0400

    Added to extract_ffmpeg_frames and collect_raw

--- Collect_Training_Code/extract_ffmpeg_frames.py
+++ Collect_Training_Code/extract_ffmpeg_frames.py
@@ -37,17 +37,17 @@ def run_processes_parallel(cmd_lst):
 
 def extract_frame_command(input_vid, frame_num, output_dir):
     if frame_num <= 0:
-        command = 'ffmpeg -loglevel error -nostats -y -threads 2 -i {:s} {:s}/frame_%06d.tif'.format(input_vid, output_dir)
+        command = 'ffmpeg -loglevel error -nostats -y -i {:s} {:s}/frame_%06d.tif'.format(input_vid, output_dir)
     else:
-        command = 'ffmpeg -loglevel error -nostats -y -threads 2 -i {:s} -vframes {:d} {:s}/depth_frame_%06d.tif'.format(input_vid, frame_num, output_dir)
+        command = 'ffmpeg -loglevel error -nostats -y -i {:s} -vframes {:d} {:s}/depth_frame_%06d.tif'.format(input_vid, frame_num, output_dir)
     #sp.call(command, shell=True)
     return command
 
 def extract_rgb_frame_command(input_vid_mp4, frame_num, output_dir):
     if frame_num <= 0:
-        command = 'ffmpeg -loglevel error -nostats -y -threads 2 -i {:s} {:s}/rgb_frame_%06d.tif'.format(input_vid_mp4, output_dir)
+        command = 'ffmpeg -loglevel error -nostats -y -i {:s} {:s}/rgb_frame_%06d.tif'.format(input_vid_mp4, output_dir)
     else:
-        command = 'ffmpeg -loglevel error -nostats -y -threads 2 -i {:s} -vframes {:d} {:s}/rgb_frame_%06d.tif'.format(input_vid_mp4, frame_num, output_dir)
+        command = 'ffmpeg -loglevel error -nostats -y -i {:s} -vframes {:d} {:s}/rgb_frame_%06d.tif'.format(input_vid_mp4, frame_num, output_dir)
     return command
 
 def options():
@@ -67,16 +67,16 @@ def main():
     cmd_lst = []
     cmd_lst.append(extract_frame_command(input_vid, frame_num, output_dir))
     cmd_lst.append(extract_rgb_frame_command(input_vid_mp4, frame_num, output_dir))
-    # p1 = multiprocessing.Process(target=run_processes_parallel, args=(cmd_lst,))
-    # p1.start()
-    # try:
-    #     p1.join()
-    # except Exception as e:
-    #     p1.terminate()
-    #     p1.join()
-    #     print("Error here: {}".format(e))
-    #     #pass
-    run_processes_parallel(cmd_lst)
+    p1 = multiprocessing.Process(target=run_processes_parallel, args=(cmd_lst,))
+    p1.start()
+    try:
+        p1.join()
+    except Exception as e:
+        p1.terminate()
+        p1.join()
+        print("Error here: {}".format(e))
+        #pass
+    #run_processes_parallel(cmd_lst)
     if platform.system() == 'Linux':
         os.system("stty echo")
 
